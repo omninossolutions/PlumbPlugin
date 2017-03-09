c41861dbd48e87003151f0aecaffa7f5564c214f
Jarvics <jarvicssoft@gmail.com>
2017-03-09 15:48:19 +0530

    Initial Commit

---
+++ PlumbPlugin/PlumbPlugin.h
@@ -0,0 +1,19 @@
+//
+//  PlumbPlugin.h
+//  PlumbPlugin
+//
+//  Created by Jarvics  on 09/03/17.
+//  Copyright © 2017 Omninos Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlumbPlugin.
+FOUNDATION_EXPORT double PlumbPluginVersionNumber;
+
+//! Project version string for PlumbPlugin.
+FOUNDATION_EXPORT const unsigned char PlumbPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlumbPlugin/PublicHeader.h>
+
+
